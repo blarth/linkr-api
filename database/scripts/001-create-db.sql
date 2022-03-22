61872e29c925599a856315f944fd3777732944d5
Felipe Ventura <fmagven93@gmail.com>
2022-03-22 20:35:19 -0300

    fix: db helper scripts

---
+++ database/scripts/001-create-db.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users (
+	"id" SERIAL PRIMARY KEY,
+	"name" TEXT NOT NULL,
+	"email" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL,
+	"image" TEXT NOT NULL
+);
+
+CREATE TABLE posts (
+	"id" SERIAL PRIMARY KEY,
+	"link" TEXT NOT NULL,
+	"postText" TEXT,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id")
+);
+
+CREATE TABLE hashtags (
+	"id" SERIAL PRIMARY KEY,
+	"name" TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE hashtagsPosts (
+	"id" SERIAL PRIMARY KEY,
+	"hashtagId" INTEGER NOT NULL REFERENCES "hashtags"("id"),
+	"postId" INTEGER NOT NULL REFERENCES "posts"("id")
+);
+
+CREATE TABLE "likesPosts" (
+	"id" serial NOT NULL,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id"),
+	"postId" INTEGER NOT NULL REFERENCES "posts"("id")
+);
+
+CREATE TABLE sessions (
+   "id" SERIAL PRIMARY KEY,
+   "token" TEXT NOT NULL UNIQUE,
+   "userId" INTEGER NOT NULL REFERENCES "users"("id")
+);
