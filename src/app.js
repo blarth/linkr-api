be86a72f06d0d44f9ed35cc7e47610d10ae3a9b3
blarth <jminocente@gmail.com>
2022-03-21 21:16:46 -0300

    feat: architecture

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express, { json } from "express";
+import router from "./routes/index.js";
+import 'dotenv/config';
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(json());
+app.use(router)
+
+
+export default router;
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is listening on port " + process.env.PORT);
+});
+
