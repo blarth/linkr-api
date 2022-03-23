f1b8cf0f547fd714812ef246fc87bda8192142b2
Rayane Ventura <rayyventura@gmail.com>
2022-03-23 20:50:35 +0100

    feat: logout user

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { postLink } from "../controllers/postController.js";
 import postSchema from "../schemas/postSchema.js";
-import { validateTokenMiddleware } from "../middlewares/validateTokenMiddlewares.js";
+import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 import validateSchemaMiddleware from "../middlewares/validateSchemaMiddleware.js";
-
 
 const postRouter = Router();
 
-postRouter.post("/timeline", validateTokenMiddleware, validateSchemaMiddleware(postSchema),postLink);
+postRouter.post(
+  "/timeline",
+  validateTokenMiddleware,
+  validateSchemaMiddleware(postSchema),
+  postLink
+);
 
 export default postRouter;
