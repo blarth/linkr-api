954059d504177147f0538b3b783df629608c81ce
Rayane Ventura <rayyventura@gmail.com>
2022-03-31 21:32:53 +0200

    merge:merges with main

--- src/controllers/searchBarController.js
+++ src/controllers/searchBarController.js
@@ -1,14 +1,14 @@
 import { getUsersToSearchBar } from "../repositories/searchBarRepository.js";
 
 export async function getSearchBar(req, res) {
-    const { name } = req.params;
-    const { user } = res.locals;
-    try{
-        const result = await getUsersToSearchBar(name, user.id);
-        res.send(result.rows);
-        return (result.rows)
-    }catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
+  const { name } = req.params;
+  const { user } = res.locals;
+  try {
+    const result = await getUsersToSearchBar(name, user.id);
+    res.send(result.rows);
+    return result.rows;
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
--- src/repositories/searchBarRepository.js
+++ src/repositories/searchBarRepository.js
@@ -1,9 +1,12 @@
 import connection from "../../db.js";
 
-export async function  getUsersToSearchBar(name){
-    return connection.query(`
+export async function getUsersToSearchBar(name) {
+  return connection.query(
+    `
     SELECT u.id, u.name, u.image, f."followedByUserId" AS "followed" FROM users u 
     LEFT JOIN "followers" f ON f."userId" =  u.id
     WHERE name like $1
-`,[`${name}%`]);
+`,
+    [`${name}%`]
+  );
 }
