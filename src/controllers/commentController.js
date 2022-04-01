3116eb7a3049733fd56f54b4311728e823467c84
LucasVz <lucastadeuvaz@gmail.com>
2022-04-01 00:14:16 -0300

    feat: postComments

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,10 +1,23 @@
-import { fetchComments } from "../repositories/commentRepository.js";
+import { fetchComments, postComment } from "../repositories/commentRepository.js";
 
 export async function getComments(req, res){
 	try{
 		const {id} = req.params;
 		const { rows: comments } = await fetchComments(id)
 		res.status(200).send(comments);
+	}catch(error){
+		console.log(error);
+		res.sendStatus(500);
+	}
+}
+
+export async function postComments(req, res){
+	const {user} = res.locals
+	const { comment } = req.body;
+	const {id} = req.params;
+	try{
+		await postComment(id, comment, user.id);
+		res.sendStatus(201);
 	}catch(error){
 		console.log(error);
 		res.sendStatus(500);
